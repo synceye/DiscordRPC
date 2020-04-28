9277d407cf61bc00f28c04c1402f1f842595cfc4
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-04-28 14:57:31 +0200

    Dodao upute sto je sto i stavio watermark
     Molim ne brisati :) 👍

--- DiscordRPC/discord.lua
+++ DiscordRPC/discord.lua
@@ -4,15 +4,15 @@ Citizen.CreateThread(function()
         Citizen.Wait(6*1000)
 
         SetDiscordAppId(660063736765087744)
-        -- Sync je na IAP
-        --SetRichPresence("ID: " .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1)))  | "Lokacija: " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player)))))
-        SetRichPresence("ID: " .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))) .. " | " .. GetPlayerName(PlayerId()) .. " je na ".. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))) 
+        -- Molim da se ostavi ova markirana poruka jer ipak sam je ja napravio ;) !
+        --SetRichPresence("ID: " .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1)))  | "Lokacija: " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))) -- Stari kod <---
+        SetRichPresence("ID: " .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))) .. " | " .. GetPlayerName(PlayerId()) .. " je na ".. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))) -- Novi kod <--- 
+                                   -- ^Prikazuje ID igraca^ --                                         -- ^Prikazuje id (nemojte dirati)^         ^Prikazuje lokaciju gdje se nalazi taj lik, updatea se svaki put kada negdje dodje^
+        SetDiscordRichPresenceAsset("VASA_SLIKA") <-- Ona velika slika na RPC
+        SetDiscordRichPresenceAssetText(GetPlayerName(PlayerId())) <-- ovo tu prikazuje ime igraca prema idu, koji je spojen na steam njihov. U isto vrijeme je to tekst koji je za veliku sliku
 
-        SetDiscordRichPresenceAsset("imageeeee")
-        SetDiscordRichPresenceAssetText(GetPlayerName(PlayerId()))
-
-        SetDiscordRichPresenceAssetSmall("discord-logo-black_1_")
-        SetDiscordRichPresenceAssetSmallText("therapyblkn.ddns.net")
+        SetDiscordRichPresenceAssetSmall("VASA_SLIKA") <-- Ona sitna, mala slika na RPC
+        SetDiscordRichPresenceAssetSmallText("therapyblkn.ddns.net") <-- Tekst koji ocete staviti
     end
 
 

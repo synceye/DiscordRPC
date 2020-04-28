05ca3953ef57dd563ba2b11d66bba09e3df429bd
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-04-28 15:43:38 +0200

    Maknuo moj Bot ID i dodao uputa još
    Ako vam i dalje nije jasno sto trebate u 6. redu, znaci, otidete na https://discordapp.com/developers/applications/ i tamo napravite APP, NEMOJTE STAVITI DA JE BOT, i onda kopirate samo ID (Client ID), treba izgledati nekako ovako: 15645646735346 otp.
    I uredite slike i stavite neka imena koja ocete

--- DiscordRPC/discord.lua
+++ DiscordRPC/discord.lua
@@ -3,7 +3,7 @@ Citizen.CreateThread(function()
         local player = GetPlayerPed(-1)
         Citizen.Wait(6*1000)
 
-        SetDiscordAppId(660063736765087744)
+        SetDiscordAppId() -- U OVE ZAGRADE STAVITE CLIENT ID OD VAŠEG DISCORD RPCa BEZ NAVODNIKA (AKO NE ZNATE ŠTO JE TO IMATE NA GOOGLE SVE, NAPISITE DISCORD DEVELOPER TUTORIAL BOT ILI TAKO NESTO)
         -- Molim da se ostavi ova markirana poruka jer ipak sam je ja napravio ;) !
         --SetRichPresence("ID: " .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1)))  | "Lokacija: " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))) -- Stari kod <---
         SetRichPresence("ID: " .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))) .. " | " .. GetPlayerName(PlayerId()) .. " je na ".. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))) -- Novi kod <--- 
